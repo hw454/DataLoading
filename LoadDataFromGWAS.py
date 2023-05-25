fb95b0a1885a37148e582ecc0d0def31b5d41100
Hayley Wragg <hayleywragg@hotmail.com>
2023-05-25 16:49:49 +0100

    Createing a csv from multiple excel sheets ready for clustering has been functionalised.

--- LoadDataFromGWAS.py
+++ LoadDataFromGWAS.py
@@ -1,12 +1,10 @@
-import ieugwaspy as igd
-import pandas as pd
-from icecream import ic
+
+import DataLoadingFunctions as dl
 import logging
 logging.basicConfig(filename='LoadDataFromGWAS.log', 
                     encoding='utf-8', 
-                    level=logging.DEBUG)
-logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s \n',
-                    level=logging.INFO,
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s \n',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 '''Python script to load data on pathway identification and GWAS 
@@ -20,8 +18,7 @@ data on the relationship to BMI and cancer outcome for the same SNPs.'''
 # This data for pathway identification is taken from: https://www.nature.com/articles/s41416-022-02060-6
 # Load this with: rsid, chr.pos, exposure, outcome, a1, a2, bx, bxse, Adipose_P, Brain_P
 path='../Python/ICEP/'
-labels=['rsid','chr.pos','exposure','outcome','a1','a2','bx','bxse','by','byse',
-        'Adipose_P','Brain_P']
+
 
 
 # Inputs for results from: 
@@ -31,139 +28,21 @@ labels=['rsid','chr.pos','exposure','outcome','a1','a2','bx','bxse','by','byse',
 # outcome_ids=['ebi-a-GCST006464','ieu-a-1120','ieu-a-1126','ieu-b-85','QyUAvp','kL0KZ3',
 # 'KBEFfw','ebi-a-GCST006464','ieu-a-1120','ieu-a-1126']
 col_labs={'rsid':'SNP','a1':'ea','a2':'nea','bx':'beta','bxse':'se','by':'beta','byse':'se',
-          'Adipose_P':'PPA4_adipose','Brain_P':'PPA4_brain','outcome':'trait','outcome_id':'id.outcome',
+          'Pathway1_suffix':'adipose','Pathway2_suffix':'brain','outcome':'trait','outcome_id':'id.outcome',
           'expo_lab':'BMI','coloc_lab':'PPA4'}
 alltables               ='HarnessingExcelTables'
 insheettyp='.xlsx'
 # Sheets within the spreadsheet containing pathway colocization scores
-pathway1_tab_list=[sheet_table('Table 4',RS=4),sheet_table('Table 7',RS=5,UC='A:G')]
-pathway2_tab_list=[sheet_table('Table 5',RS=4),sheet_table('Table 7',RS=5,UC='K:Q')]
+pathway1_tab_list=[dl.sheet_table('Table 4',RS=4),dl.sheet_table('Table 7',RS=5,UC='A:G')]
+pathway2_tab_list=[dl.sheet_table('Table 5',RS=4),dl.sheet_table('Table 7',RS=5,UC='K:Q')]
 # Sheets within the spreadsheet containing the outcome_ids
-outcome_id_tab_list=[sheet_table('Table 13',4,24),sheet_table('Table 14', 5,37),sheet_table('Table 18',3,41),sheet_table('Table 18',43,69)]
-expo_tab_list      =[sheet_table('Table 16',3)]
-class sheet_table:
-    def __init__(s,SN,RS=None,NT=None,UC=None):
-        '''
-        :param SN: string indicating sheetname
-        :param RS: Integer indicating number of rows to skip
-        :param NT: Total number of rows being searched for
-        :param UC: Collumns to be used'''
-        s.sheet_name=SN
-        s.row_skip  =RS
-        s.nrows     =NT
-        s.cols      =UC
-    def load(s,fname):
-        df=pd.read_excel(fname,sheet_name=s.SN,skiprows=s.row_skip,nrows=s.nrows,usecols=s.cols)
-        return df
-def load_tab_list(tab_list,fname):
-    '''' Iterate through the items in tab_list. Load the dataframe for each then merge. Duplicate rows will be deleted.'''
-    df_tot=pd.DataFrame() 
-    for tl in tab_list:
-        df_single=tl.load(fname)
-        df_tot=pd.concat([df_single,df_tot],ignore_index=True)
-    df_tot.drop_duplicates(keep='first',ignore_index=True,inplace=True)
-    return df_tot
-
-def Load_dfs(col_labs,pathway1_tab_list,pathway2_tab_list,outcome_id_tab_list,expo_tab_list):
-    '''
-    :param col_loabs: dictionary of strings matching the column labels to desired information.
-    :param pathway1_tab_list: list of the information for loading each table related to pathway1 in :py:class:`sheet_table` type.
-    :param pathway2_tab_list: list of the information for loading each table related to pathway2 in :py:class:`sheet_table` type
-    :param outcome_id_tab_list: list of the information for loading each table related to outcome ids in :py:class:`sheet_table` type.
-    :param expo_tab_list: list of the information for loading each table related to exposures in :py:class:`sheet_table` type'''
-# Set the labels based on the inputs
-a1_lab      =col_labs['a1']
-a2_lab      =col_labs['a2']
-by_lab      =col_labs['by']
-byse_l      =col_labs['byse']
-rsid_lab    =col_labs['rsid']
-adi_lab     =col_labs['Adipose_P']
-bri_lab     =col_labs['Brain_P']
-outcome_lab =col_labs['outcome_id']
-bxlab       =col_labs['bx']
-bxselab     =col_labs['bxse']
+outcome_id_tab_list=[dl.sheet_table('Table 13',4,24),dl.sheet_table('Table 14', 5,37),dl.sheet_table('Table 18',3,41),dl.sheet_table('Table 18',43,69)]
+expo_tab_list      =[dl.sheet_table('Table 16',3)]
 
 
 logging.info('Manual inputs made')
 logging.info('-----------------------')
-#Load Adipose and Brain colocalization scores.
-adi_pathway_df  =pd.read_excel(path+alltables+insheettyp,sheet_name=Adipose_Sheet,skiprows=nskippath)[[rsid_lab,coloc_lab]]
-brain_pathway_df=pd.read_excel(path+alltables+insheettyp,sheet_name=Brain_Sheet,skiprows=nskippath)[[rsid_lab,coloc_lab]]
-pathway_df      =pd.merge(adi_pathway_df,brain_pathway_df,how='inner',on=rsid_lab,suffixes=('_brain','_adipose'))
-pathway_df      =pathway_df[['SNP',adi_lab,bri_lab]]
-pathway_df.rename(columns={rsid_lab:'rsid'},inplace=True)
 
-logging.info('Data loaded from excel spreadshet '+path+alltables+insheettyp)
-# Load the adipose and brain borderline scores. 
-adi_bord_path_df=pd.read_excel(path+alltables+insheettyp,sheet_name=pathway_borderline_sheet,
-                              skiprows=nskip_bor,usecols='A:G')[[rsid_lab,coloc_lab]]
-bri_bord_path_df=pd.read_excel(path+alltables+insheettyp,sheet_name=pathway_borderline_sheet,
-                              skiprows=nskip_bor,usecols='K:Q')[[rsid_lab+'.1',coloc_lab+'.1']]
-bri_bord_path_df.rename(columns={rsid_lab+'.1':rsid_lab,coloc_lab+'.1':coloc_lab},inplace=True)
-# Combine the borderline scores
-border_path_df  =pd.merge(adi_bord_path_df,bri_bord_path_df,how='inner',on=rsid_lab,suffixes=('_brain','_adipose'))
-border_path_df=border_path_df[[rsid_lab,adi_lab,bri_lab]]
-border_path_df.rename(columns={rsid_lab:'rsid'},inplace=True)
-# Make one full dataframe for colocization scores on both pathways
-pathway_df=pd.concat([pathway_df,border_path_df])
-
-logging.info('Pathway snp level data loaded')
-
-# Retrieve the outcome_ids for getting the outcome results from open GWAS.
-outcome_id_df=pd.read_excel(path+alltables+insheettyp,sheet_name=Outcome_id_Sheet,skiprows=nskip_outcome,nrows=noutcomes-nskip_outcome)[outcome_lab]
-outcome_id_mo=pd.read_excel(path+alltables+insheettyp,sheet_name=Outcome_id_Sheet2,skiprows=nskip_outcome2)[outcome_lab]
-outcome_id_th=pd.read_excel(path+alltables+insheettyp,sheet_name=Outcome_id_Sheet3,skiprows=nskip_outcome3,nrows=noutcomes3-nskip_outcome3)[outcome_lab]
-outcome_id_fo=pd.read_excel(path+alltables+insheettyp,sheet_name=Outcome_id_Sheet3,skiprows=nskip_outcome4)[outcome_lab]
-# Combine all the outcome ids found in the spreadsheet and remove duplicates.
-outcome_id_df=pd.concat([outcome_id_df,outcome_id_mo,outcome_id_th,outcome_id_fo])
-outcome_id_df.drop_duplicates(keep='first',inplace=True)
-outcome_ids=outcome_id_df.values.tolist()
-logging.info('Outcome ids loaded')
-
-# Load the SNP level data for the exposure
-expo_df=pd.read_excel(path+alltables+insheettyp,sheet_name=expo_sheet,skiprows=nskip_expo)[[rsid_lab,bxlab,bxselab]]
-expo_df.rename(columns={rsid_lab:'rsid',bxlab:'bx',bxselab:'bxse'},inplace=True)
-logging.info('Exposure snp level data loaded')
-
-ic(expo_df.head())
-ic(outcome_ids)
-# Load the snp level data for the outcomes from open gwas
-logging.info('-----------------------')
-logging.info('Accessing Open GWAS')
-assocs=pd.DataFrame.from_dict(igd.associations(expo_df['rsid'],outcome_ids,align_alleles=1, 
-                                                 palindromes=1))
-logging.info('Outcome snp level data loaded from open GWAS')
-logging.info('-----------------------')
-ic(assocs.head())
-# Create the output dataframe for the snp level data.
-# Required form: 
-# rsid, chr.position, exposure, outcome, a1, a2, bx, xse, by, byse, adipose_ppa4, brain_ppa4 
-logging.info('Reformat the data into readible csv')
-logging.info('CSV column labels: '+str(labels))
-out_df=pd.DataFrame(columns=labels)
-out_df['rsid']=assocs['rsid']
-out_df['exposure']=expo_lab
-out_df['chr.pos']=['chr'+str(c)+':'+str(p) for c,p in zip(assocs['chr'],assocs['position'])]
-out_df['a1']    =assocs[a1_lab]
-out_df['a2']    =assocs[a2_lab]
-out_df['by']    =assocs[by_lab]
-out_df['byse']  =assocs[byse_l]
-out_df['outcome']=[w.replace(" ","_") for w in assocs['trait']]
-
-# Matche the outcome and exposure pairs to fill in the bx, bxse and PPA4 scores.
-for rid in expo_df['rsid']:
-    out_df.loc[out_df.rsid==rid,'bx']       =expo_df.loc[expo_df.rsid==rid,    'bx'].item()
-    out_df.loc[out_df.rsid==rid,'bxse']     =expo_df.loc[expo_df.rsid==rid,    'bxse'].item()
-for rid in pathway_df['rsid']:
-    try: 
-        out_df.loc[out_df.rsid==rid,'Adipose_P']=pathway_df.loc[pathway_df.rsid==rid,'PPA4_adipose'].item()
-    except: pass
-    try: 
-        out_df.loc[out_df.rsid==rid,'Brain_P']  =pathway_df.loc[pathway_df.rsid==rid,'PPA4_brain'].item()
-    except: pass
-print(out_df.head())
-out_df.to_csv(path+alltables+'FullResults.csv',index=False)
-print('file at',path+alltables+'FullResults.csv')
 
 # GET THE PATHWAY MR RESULTS FOR HYPOTHESIS TESTING
 # Set the input parameters based on the setup of the spreadsheet in the supplementary data
